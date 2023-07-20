81d99aef7991b6f05179332b4e7a5f4b374cefe6
Loïc Pernot <lac.pernot@gmail.com>
2023-07-20 14:29:07 +0200

    Implemented dash
    Got dash to work
    Added momentum conservation on dashes
    Fixed an issue with double/wall jumps where downards momentum was being conserved

--- 2D/PlatformSample/Assets/Scripts/PlatformPlayerData.cs
+++ 2D/PlatformSample/Assets/Scripts/PlatformPlayerData.cs
@@ -20,30 +20,42 @@ public class PlatformPlayerData : ScriptableObject
         [Range(0.01f, 1)] public float airDeccelModifier; /* The modifier applied to air decceleration. One more note: these are currently contained between 0 and 1,
                                                             but your air accelaration absolutely could be higher than your ground acceleration.*/
         public bool conserveMomentum;
+
         [Header("Upwards movement")]
         public float jumpPower; // The amount of force (impulse) to add to the player when they jump.
         public float jumpGravity; // Helps making the player fall faster without affecting their gravity.
-        public float fallGravityModifier;
-        public float hangGravityModifier;
-        public float hangAbsVelocity;
+        public float fallGravityModifier; // How much higher should the player's gravity when they fall.
+        public float maxFallSpeed; // What's the fast the player may fall.
+        public float hangGravityModifier; // How much to cut the gravity when in the jump's hang time.
+        public float hangAbsVelocity; // What speeds to consider as the jump's hang time.
         [Range(0.1f, 1)] public float jumpSlowdown; // How hard should the player slow down once they release the jump button.
+
         [Header("Input buffering")]
         [Range(0.1f, 1)] public float coyotteTime; // Time (in seconds) after leaving a platform during which the player can still perform a jump.
         [Range(0.1f, 1)] public float jumpBufferTime; // How long the input buffer for jumps should be before hitting the ground.
+
         [Header("Wall sliding & wall jumps")]
-        public float wallSlidingSpeed;
-        public float wallKickOff;
-        public float wallJumpPower;
-        [Range(0.1f, 1)] public float wallAccelModifier;
-        [Range(0.1f, 1)] public float wallAccelTime;
+        public float wallSlidingSpeed; // How fast should the player be going down when they are wallsliding.
+        public float wallKickOff; // How far back should the player be pushed when walljumping.
+        public float wallJumpPower; // How much height should they get from a wall jump.
+        public float wallDashPower;
+        /* Next two variables need to be explained together. Basically, when doing a wall jump, we reduce the player's air acceleration by wallAccelModifier for a short duration
+           wallAccelTime. This allows us to control how easily a player can get back to the same wall they jumped off from without kicking them too far back. Setting the duration
+           to 0 and the modifier to 1, and with sufficient air acceleration, the player may be able to reach the wall higher than where they left from.*/
+        [Range(0f, 1)] public float wallAccelModifier;
+        [Range(0f, 1)] public float wallAccelTime;
+
         [Header("Double jumping")]
-        public float doubleJumpPower;
-        public int doubleJumpMax;
+        public float doubleJumpPower; // How much height should the player get from a double jump.
+        public int doubleJumpMax; // How many double jumps can a player perform without landing to replenish.
+
         [Header("Dashing")]
-        public float dashPower;
-        public float dashDuration;
-        public float dashCooldown;
-        public int dashMax;
+        // Both dashPower and dashDuration will change how far a dash travels, but dashPower will also provide momentum to player if they jump-cancel a dash.
+        public float dashPower; // How much impulse should a dash provide.
+        public float dashDuration; // How long should a dash last.
+        public float dashGraceTime; // A tiny grace period during which gravity is still off so the player can react to the dash.
+        public float dashCooldown; // How long after dashing can a player dash again.
+        public int dashMax; // How many dashes can a player perform without landing to replenish.
 
     private void OnValidate()
     {
--- 2D/PlatformSample/Assets/Scripts/PlatformPlayerScript.cs
+++ 2D/PlatformSample/Assets/Scripts/PlatformPlayerScript.cs
@@ -31,20 +31,6 @@ public class PlatformPlayerScript : MonoBehaviour
     public LayerMask groundLayer;
     #endregion
 
-    public float latSpeed;
-    public float timeToSpeed;
-    public float jumpStr;
-    public float jumpSlowdown = 0.5f;
-    public float coyotteTime = 0.2f;
-    public float jumpBufferTime = 0.2f;
-    public float wallSlidingSpeed = 4f;
-    public float wallJumpTime = 0.4f;
-    public float wallJumpPushBack;
-    public float wallJumpStr;
-    public float wallJumpSlowdown = 0.5f;
-    public float doubleJumpStr;
-    public int doubleJumpMax;
-
     private PlayerInputScript input;
     private Vector2 movementVector = Vector2.zero;
 
@@ -54,7 +40,9 @@ public class PlatformPlayerScript : MonoBehaviour
     private bool isJumping;
     private bool isWallJumping;
     private bool isDoubleJumping;
+    private bool wantsDash;
     private bool isDashing;
+    private bool isDashCancel;
     #endregion
 
     #region Timers
@@ -94,27 +82,13 @@ public class PlatformPlayerScript : MonoBehaviour
 
     private void DashPerformed(InputAction.CallbackContext context)
     {
-        if (dashCounter > 0)
-        {
-            #region Getting dash direction
-            Vector2 dashDirection;
-            if (movementVector == Vector2.zero)
-                dashDirection = new Vector2(transform.localScale.x, 0);
-            else
-                dashDirection = movementVector.normalized;
-            #endregion
-
-            dashCounter -= 1;
-            isDashing = true;
-            dashCooldownTimer = Data.dashCooldown;
-
-            Invoke(nameof(DashEnded), Data.dashDuration);
-        }
+        wantsDash = true;
     }
 
-    private void DashEnded()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        isDashing = false;
+        isDashCancel = false;
+        Debug.Log(isDashCancel);
     }
 
     /* Legacy, from the old way jumps were performed. Since these functions only ever executed when the jump input was pressed,
@@ -169,14 +143,13 @@ public class PlatformPlayerScript : MonoBehaviour
         #endregion
 
         #region Momentum conservation
-        /* Commented out because I do not want to think about that right now
-        if(Data.conserveMomentum && Mathf.Abs(prb.velocity.x) > Mathf.Abs(targetSpeed) && Mathf.Sign(prb.velocity.x) == Mathf.Sign(targetSpeed) && targetSpeed > 0.01f && isGrounded())
-        {
-            //Prevent any deceleration, or conserve momentum. Might want to look into allowing players to speed up slightly.
+        //Prevent any deceleration, or conserve momentum. Might want to look into allowing players to speed up slightly.
+        if (Data.conserveMomentum && Mathf.Abs(prb.velocity.x) > Mathf.Abs(targetSpeed) && Mathf.Sign(prb.velocity.x) == Mathf.Sign(targetSpeed) && targetSpeed > 0.01f && isGrounded())
             accelRate = 0;
-        }
-        */
         #endregion
+
+        if (Data.conserveMomentum && isDashCancel && (Mathf.Sign(prb.velocity.x) == Mathf.Sign(targetSpeed) || targetSpeed == 0))
+            accelRate = 0;
 
         #region Calculating acceleration & force
         float speedDif = targetSpeed - prb.velocity.x; // Difference between current velocity and desired velocity
@@ -210,13 +183,18 @@ public class PlatformPlayerScript : MonoBehaviour
     {
         jumpBufferCounter = 0f;
         isJumping = true;
+        if (isDashing)
+        {
+            isDashCancel = true;
+            isDashing = false;
+        }
         prb.AddForce(Vector2.up * Data.jumpPower, ForceMode2D.Impulse);
     }
 
     private void JumpCancel()
     {
         jumpCancelBufferCounter = 0;
-        prb.velocity = new Vector2(prb.velocity.x, prb.velocity.y * jumpSlowdown);
+        prb.velocity = new Vector2(prb.velocity.x, prb.velocity.y * Data.jumpSlowdown);
     }
 
     private void WallSlide()
@@ -224,7 +202,7 @@ public class PlatformPlayerScript : MonoBehaviour
         if (isTouchingWall() && !isGrounded() && movementVector.x != 0f)
         {
             isWallSliding = true;
-            prb.velocity = new Vector2(prb.velocity.x, Mathf.Clamp(prb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+            prb.velocity = new Vector2(prb.velocity.x, Mathf.Clamp(prb.velocity.y, -Data.wallSlidingSpeed, float.MaxValue));
         }
         else
         {
@@ -237,7 +215,18 @@ public class PlatformPlayerScript : MonoBehaviour
         jumpBufferCounter = 0f;
         isWallJumping = true;
         float jumpDirection = -transform.localScale.x;
-        prb.AddForce(new Vector2(jumpDirection * (Data.wallKickOff), Data.wallJumpPower), ForceMode2D.Impulse);
+        if (isDashing && prb.velocity.y > 0f)
+        {
+            prb.AddForce(new Vector2(jumpDirection * (Data.wallKickOff), Data.wallDashPower), ForceMode2D.Impulse);
+            //isDashCancel = true;
+            isDashing = false;
+        }
+        else
+        {
+            prb.velocity = new Vector2(prb.velocity.x, 0);
+            prb.AddForce(new Vector2(jumpDirection * (Data.wallKickOff), Data.wallJumpPower), ForceMode2D.Impulse);
+        }
+
         Invoke(nameof(CancelWallJump), Data.wallAccelTime);
     }
 
@@ -251,7 +240,53 @@ public class PlatformPlayerScript : MonoBehaviour
         jumpBufferCounter = 0;
         doubleJumpCounter -= 1;
         isDoubleJumping = true;
+        if (prb.velocity.y < 0f)
+            prb.velocity = new Vector2(prb.velocity.x, 0);
         prb.AddForce(Vector2.up * Data.doubleJumpPower, ForceMode2D.Impulse);
+    }
+
+    private void Dash()
+    {
+        #region Setting all state parameters
+        wantsDash = false;
+        dashCounter -= 1;
+        isDashing = true;
+        dashCooldownTimer = Data.dashCooldown;
+        #endregion
+
+        #region Getting dash direction
+        Vector2 dashDirection;
+        if (movementVector == Vector2.zero)
+            dashDirection = new Vector2(transform.localScale.x, 0);
+        else
+            dashDirection = movementVector.normalized;
+        #endregion
+
+        #region Doing physics
+        prb.velocity = Vector2.zero; // Necessary so previously existing upwards momentum doesn't mess with the dash
+        prb.gravityScale = 0; // Necessary so that dash travels in a straight line
+        prb.AddForce(dashDirection * Data.dashPower, ForceMode2D.Impulse);
+        #endregion
+
+        Invoke(nameof(DashEnded), Data.dashDuration);
+    }
+
+    private void DashEnded()
+    {
+        if (isDashing)
+        {
+            Debug.Log("I ran");
+            prb.velocity = Vector2.zero;
+            if (!isGrounded())
+                Invoke(nameof(DashGraceEnded), Data.dashGraceTime);
+            else
+                isDashing = false;
+        }
+    }
+
+    private void DashGraceEnded()
+    {
+        isDashing = false;
     }
 
     // Start is called before the first frame update
@@ -281,6 +316,23 @@ public class PlatformPlayerScript : MonoBehaviour
         }
         #endregion
 
+        #region Setting gravity scale based on situation
+        // If the player has no vertical velocity (i.e. they are grounded) or going upwards, gravity is lessened.
+        if (prb.velocity.y >= 0f)
+            prb.gravityScale = Data.jumpGravity;
+        // If the player is moving downwards (i.e. they are falling), the gravity is raised.
+        else if (prb.velocity.y < 0f)
+            prb.gravityScale = Data.jumpGravity * Data.fallGravityModifier;
+        /* If the player is currently jumping, and their vertical velocity is low
+           (i.e. they are at the apex of their jump), gravity is drastically reduced to give them more hang time. */
+        if ((isJumping || isWallJumping || isDoubleJumping) && Mathf.Abs(prb.velocity.y) < Data.hangAbsVelocity)
+            prb.gravityScale *= Data.hangGravityModifier;
+        // If the player is currently performing a dash, there is no gravity at all.
+        if (isDashing)
+            prb.gravityScale = 0;
+        /* Note that because of the order of if statements, the priority always goes dash gravity > hang time gravity > rising/falling gravity.*/
+        #endregion
+
         /* This part runs the actual jump. It only executes if the jump buffer counter is still higher than zero (so if you're
         still within the timeframe to buffer a jump after pressing), and you have coyotte time left (i.e. you just left the ground
         or are currently grounded).*/
@@ -294,7 +346,7 @@ public class PlatformPlayerScript : MonoBehaviour
             {
                 Jump();
             }
-            else if (doubleJumpCounter > 0 && !canBuffer())
+            else if (doubleJumpCounter > 0 && !canBuffer() && !isDashing)
             {
                 DoubleJump();
             }
@@ -303,20 +355,19 @@ public class PlatformPlayerScript : MonoBehaviour
         /* This part runs if the jump was canceled (i.e. the key was realeased) within the buffer time frame, and the player is moving upwards.
         It works by multiplying the upwards velocity of the player by something lesser than 1 (i.e. dramatically slowing down their ascent).
         It doesn't allow the player to un-cancel their jump right before landing, but that could certainly be implemented, if necessary.*/
-        if (jumpCancelBufferCounter > 0f && prb.velocity.y > 0f)
+        if (jumpCancelBufferCounter > 0f && prb.velocity.y > 0f && !isWallJumping)
         {
             JumpCancel();
         }
 
-        Move();
+        if (wantsDash && dashCooldownTimer < 0f && dashCounter > 0)
+            Dash();
 
-        if (prb.velocity.y > 0f && !isDashing)
-            prb.gravityScale = Data.jumpGravity;
-        else if (prb.velocity.y <= 0f && isDashing!)
-            prb.gravityScale = Data.jumpGravity * Data.fallGravityModifier;
-        if ((isJumping || isWallJumping || isDoubleJumping) && Mathf.Abs(prb.velocity.y) < Data.hangAbsVelocity && !isDashing)
-            prb.gravityScale *= Data.hangGravityModifier;
+        if (!isDashing)
+            Move();
 
+        if (prb.velocity.y < 0f)
+            prb.velocity = new Vector2(prb.velocity.x, Mathf.Max(-Data.maxFallSpeed, prb.velocity.y));
     }
 
     // Update is called once per frame
